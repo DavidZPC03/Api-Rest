43de479ae47f5d0a1ab510e4fc698e51fe1025f7
DavidZPC03 <davidzpc03@gmail.com>
2025-01-31 09:42:08 -0600

    "Server Express Hello World in class"

---
+++ Parcial1/4_ServerExpress/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send(__dirname + '/public.index.html');
+    next();
+    });
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request');
+    next();
+    });
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    });
+
