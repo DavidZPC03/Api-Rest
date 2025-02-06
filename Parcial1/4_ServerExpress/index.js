e5019a3a66237a7b78ef0f2dd5b143718c95a710
DavidZPC03 <davidzpc03@gmail.com>
2025-02-06 09:06:22 -0600

    'Middleware Funcion'

--- Parcial1/4_ServerExpress/index.js
+++ Parcial1/4_ServerExpress/index.js
@@ -2,6 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use('/',(req, res, next) => {
+    console.log("peticion al server");
+    next();
+},(req, res, next) => {
+    console.log("2da peticion al middleware");
+    next();
+    });
+
+//Middleware incorporado en express
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Got a POST request');
+    }
+);
+
+app.use((req,res,) => {
+    res.status(404);
+    res.send('Error 404');
+    });
+
 app.get('/', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.sendFile( __dirname + '/public/index.html');
