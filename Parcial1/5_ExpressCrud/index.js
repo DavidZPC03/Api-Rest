397534c0906aa69f2ca182650870ef4f2a3bab69
DavidZPC03 <davidzpc03@gmail.com>
2025-04-28 12:00:59 -0500

    Add Swagger documentation dependencies to package.json

--- Parcial1/5_ExpressCrud/index.js
+++ Parcial1/5_ExpressCrud/index.js
@@ -4,6 +4,17 @@ const cors = require('cors');
 const empleadosRouter = require('./routes/empleadosRouter');
 const router = require('./routes/empleadosRouter');
 const hateoasLinker = require('express-hateoas-links');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const path = require('node:path');
+const swaggerOptions = require('./swaggerOptions.json');
+
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs));
+app.get("/api-docs-json", (req, res) => {
+    res.json(swaggerDocs);
+});
 
 app.use(cors());
 app.use(hateoasLinker)
--- Parcial1/5_ExpressCrud/routes/empleadosRouter.js
+++ Parcial1/5_ExpressCrud/routes/empleadosRouter.js
@@ -1,16 +1,246 @@
 const express = require('express');
 const router = express.Router();
 
-
 const createEmpleado = require('../controller/createEmpleadoController.js');
 const getEmpleados = require('../controller/getEmpleadoController.js');
 const updateEmpleado = require('../controller/updateEmpleadoController.js');
 const deleteEmpleado = require('../controller/deleteEmpleadoController.js');
 
-//se utiliza el mismo endpoint y solamente cambio los metodos para cada operacion
-router.post('/empleados', createEmpleado);
-router.get('/empleados', getEmpleados);
-router.put('/empleados', updateEmpleado);
-router.delete('/empleados', deleteEmpleado);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Empleado:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ *         nombre:
+ *           type: string
+ *           example: Gerardo
+ *         edad:
+ *           type: integer
+ *           example: 29
+ *         pais:
+ *           type: string
+ *           example: México
+ *         cargo:
+ *           type: string
+ *           example: QA
+ *         anios:
+ *           type: integer
+ *           example: 5
+ *     EmpleadoInput:
+ *       type: object
+ *       required:
+ *         - nombre
+ *         - edad
+ *         - pais
+ *         - cargo
+ *         - anios
+ *       properties:
+ *         nombre:
+ *           type: string
+ *           example: Gerardo
+ *         edad:
+ *           type: integer
+ *           example: 29
+ *         pais:
+ *           type: string
+ *           example: México
+ *         cargo:
+ *           type: string
+ *           example: QA
+ *         anios:
+ *           type: integer
+ *           example: 5
+ *     EmpleadoUpdate:
+ *       type: object
+ *       required:
+ *         - id
+ *         - nombre
+ *         - edad
+ *         - pais
+ *         - cargo
+ *         - anios
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ *         nombre:
+ *           type: string
+ *           example: Gerardo Actualizado
+ *         edad:
+ *           type: integer
+ *           example: 31
+ *         pais:
+ *           type: string
+ *           example: México
+ *         cargo:
+ *           type: string
+ *           example: Líder Técnico
+ *         anios:
+ *           type: integer
+ *           example: 7
+ *     EmpleadoDelete:
+ *       type: object
+ *       required:
+ *         - id
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Empleados
+ *   description: Operaciones CRUD para empleados
+ */
+
+/**
+ * @swagger
+ * /empleados:
+ *   post:
+ *     summary: Crear un nuevo empleado
+ *     tags: [Empleados]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/EmpleadoInput'
+ *     responses:
+ *       201:
+ *         description: Empleado creado exitosamente
+ *     x-codeSamples:
+ *       - lang: JavaScript
+ *         source: |
+ *           const axios = require('axios');
+ *           axios.post('http://localhost:3001/empleados', {
+ *             nombre: 'Gerardo',
+ *             edad: 29,
+ *             pais: 'México',
+ *             cargo: 'QA',
+ *             anios: 5
+ *           }).then(response => {
+ *             console.log(response.data);
+ *           }).catch(error => {
+ *             console.error(error);
+ *           });
+ *       - lang: cURL
+ *         source: |
+ *           curl -X POST http://localhost:3001/empleados \
+ *           -H "Content-Type: application/json" \
+ *           -d '{"nombre": "Gerardo", "edad": 29, "pais": "México", "cargo": "QA", "anios": 5}'
+ */
+router.post('/', createEmpleado);
+
+/**
+ * @swagger
+ * /empleados:
+ *   get:
+ *     summary: Obtener la lista de empleados
+ *     tags: [Empleados]
+ *     responses:
+ *       200:
+ *         description: Lista de empleados obtenida exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Empleado'
+ *     x-codeSamples:
+ *       - lang: JavaScript
+ *         source: |
+ *           const axios = require('axios');
+ *           axios.get('http://localhost:3001/empleados')
+ *             .then(response => {
+ *               console.log(response.data);
+ *             })
+ *             .catch(error => {
+ *               console.error(error);
+ *             });
+ *       - lang: cURL
+ *         source: |
+ *           curl -X GET http://localhost:3001/empleados
+ */
+router.get('/', getEmpleados);
+
+/**
+ * @swagger
+ * /empleados:
+ *   put:
+ *     summary: Actualizar un empleado existente
+ *     tags: [Empleados]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/EmpleadoUpdate'
+ *     responses:
+ *       200:
+ *         description: Empleado actualizado exitosamente
+ *     x-codeSamples:
+ *       - lang: JavaScript
+ *         source: |
+ *           const axios = require('axios');
+ *           axios.put('http://localhost:3001/empleados', {
+ *             id: 1,
+ *             nombre: 'Gerardo Actualizado',
+ *             edad: 31,
+ *             pais: 'México',
+ *             cargo: 'Líder Técnico',
+ *             anios: 7
+ *           }).then(response => {
+ *             console.log(response.data);
+ *           }).catch(error => {
+ *             console.error(error);
+ *           });
+ *       - lang: cURL
+ *         source: |
+ *           curl -X PUT http://localhost:3001/empleados \
+ *           -H "Content-Type: application/json" \
+ *           -d '{"id": 1, "nombre": "Gerardo Actualizado", "edad": 31, "pais": "México", "cargo": "Líder Técnico", "anios": 7}'
+ */
+router.put('/', updateEmpleado);
+
+/**
+ * @swagger
+ * /empleados:
+ *   delete:
+ *     summary: Eliminar un empleado
+ *     tags: [Empleados]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/EmpleadoDelete'
+ *     responses:
+ *       200:
+ *         description: Empleado eliminado exitosamente
+ *     x-codeSamples:
+ *       - lang: JavaScript
+ *         source: |
+ *           const axios = require('axios');
+ *           axios.delete('http://localhost:3001/empleados', {
+ *             data: { id: 1 }
+ *           }).then(response => {
+ *             console.log(response.data);
+ *           }).catch(error => {
+ *             console.error(error);
+ *           });
+ *       - lang: cURL
+ *         source: |
+ *           curl -X DELETE http://localhost:3001/empleados \
+ *           -H "Content-Type: application/json" \
+ *           -d '{"id": 1}'
+ */
+router.delete('/', deleteEmpleado);
 
 module.exports = router;
