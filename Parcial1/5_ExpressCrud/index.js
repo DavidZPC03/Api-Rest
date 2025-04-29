d915ae6cbbaca02c8a9d4294e512092a96140d22
DavidZPC03 <davidzpc03@gmail.com>
2025-04-29 09:29:32 -0500

    Add redoc-express for API documentation and update package.json and package-lock.json

--- Parcial1/5_ExpressCrud/index.js
+++ Parcial1/5_ExpressCrud/index.js
@@ -8,13 +8,17 @@ const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const path = require('node:path');
 const swaggerOptions = require('./swaggerOptions.json');
-
+const redoc = require('redoc-express')
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 app.get("/api-docs-json", (req, res) => {
     res.json(swaggerDocs);
 });
+
+app.get('/redoc', 
+    redoc({ title: 'API Docs', specUrl: '/api-docs-json' }),
+)
 
 app.use(cors());
 app.use(hateoasLinker)
